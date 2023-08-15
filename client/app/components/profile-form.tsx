bcff01404eabc5595887944505afffcf0fbc9ff4
Philippa Payne <philippacrpayne@yahoo.com>
2023-08-15 12:38:37 +0200

    FEAT: handle submission of form

--- client/app/components/profile-form.tsx
+++ client/app/components/profile-form.tsx
@@ -1,22 +1,74 @@
 import React, { useState } from 'react';
 
 export default function ProfileForm() {
-  function handleClick() {
-    // Handle click should be handle submits
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [bio, setBio] = useState('');
+
+  async function handleSubmit() {
+    await fetch('localhost:3001/create-profile', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({firstName, lastName, email, bio}),
+    }).then((res) => res.json());
+    setFirstName('');
+    setLastName('');
+    setEmail('');
+    setBio('');
+  }
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    switch (event.target.name) {
+      case 'first-name':
+        setFirstName(event.target.value);
+      case 'last-name':
+        setLastName(event.target.value);
+      case 'email':
+        setEmail(event.target.value);
+      case 'bio':
+        setBio(event.target.value);
+    }
   }
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="first-name">First name</label>
-        <input placeholder="Jane" name="first-name" type="text" />
+        <input
+          onChange={() => handleChange}
+          value={firstName}
+          placeholder="Jane"
+          name="first-name"
+          type="text"
+        />
         <label htmlFor="last-name">Last name</label>
-        <input placeholder="Doe" name="last-name" type="text" />
+        <input
+          onChange={() => handleChange}
+          value={lastName}
+          placeholder="Doe"
+          name="last-name"
+          type="text"
+        />
         <label htmlFor="email">Email</label>
-        <input placeholder="you@email.com" name="email" type="email" />
+        <input
+          onChange={() => handleChange}
+          value={email}
+          placeholder="you@email.com"
+          name="email"
+          type="email"
+        />
         <label htmlFor="bio">Bio</label>
-        <input placeholder="Describe yourself..." name="bio" type="text" />
+        <input
+          onChange={() => handleChange}
+          value={bio}
+          placeholder="Describe yourself..."
+          name="bio"
+          type="text"
+        />
       </form>
-      <button type="submit" form="skill-form" onClick={handleClick}>
+      <button type="submit" form="skill-form">
         Save
       </button>
     </>
--- client/pages/create-profile.tsx
+++ client/pages/create-profile.tsx
@@ -2,20 +2,11 @@
 
 import React, { useState } from 'react';
 import ProfileForm from '@/app/components/profile-form';
-import SkillForm from '@/app/components/skill-form';
 
 export default function CreateProfile() {
-  const [addSkill, setAddSkill] = useState(false);
-  function handleClick() {
-    setAddSkill(true);
-  }
   return (
     <main>
       <ProfileForm />
-      <button type="submit" onClick={handleClick}>
-        Save
-      </button>
-      {addSkill && <SkillForm addSkill={addSkill} setAddSkill={setAddSkill} />}
     </main>
   );
 }
