06f7e24fbe6e35ee3fdaed4e5fba8a5cbc40530e
Philippa Payne <philippacrpayne@yahoo.com>
2023-08-19 15:10:39 +0200

    FEAT: beginning structure for job board

---
+++ client/app/components/jobs-item.tsx
@@ -0,0 +1,5 @@
+export default function JobsItem() {
+  return (
+
+  )
+}
--- client/pages/jobs.tsx
+++ client/pages/jobs.tsx
@@ -1,5 +1,21 @@
+import JobBoard from '@/app/components/job-board';
+import NavBar from '@/app/components/nav-bar';
+import { useState } from 'react';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+});
+
 export default function Jobs() {
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
-    <div>Jobs!</div>
-  )
+    <main>
+      <div className={roboto.className}>
+        <NavBar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+        <JobBoard />
+      </div>
+    </main>
+  );
 }
